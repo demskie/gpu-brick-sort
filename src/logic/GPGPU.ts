c2a362bf2e90592e289bab560c2c1ced351d5fc5
Alex Demskie <alex.demskie@gmail.com>
2019-08-03 20:28:07 -0700

    untested

--- src/logic/GPGPU.ts
+++ src/logic/GPGPU.ts
@@ -1,4 +1,17 @@
-import * as THREE from "three";
+// import * as THREE from "three";
+import * as twgl from "twgl.js";
+
+var glctx: WebGLRenderingContext | undefined;
+
+export function getWebGLContext() {
+	if (glctx) return glctx;
+	const canvas = document.createElement("canvas");
+	if (canvas) {
+		const ctx = canvas.getContext("webgl");
+		if (ctx) return (glctx = ctx);
+	}
+	return (glctx = require("gl")(0, 0) as WebGLRenderingContext);
+}
 
 const passThruVert = `
 precision highp float;
@@ -20,59 +33,142 @@ void main() {
 	gl_FragColor = texture2D(u_inputTexture, uv);
 }`;
 
-const CAMERA = new THREE.Camera();
-CAMERA.position.z = 1;
-const PASS_THRU_SHADER = createShaderMaterial(passThruFrag);
-PASS_THRU_SHADER.uniforms.texture = { value: null };
-const SCENE = new THREE.Scene();
-const MESH = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2), PASS_THRU_SHADER);
-SCENE.add(MESH);
-
-export function testCapabilities(rdr: THREE.WebGLRenderer) {
-	return null;
-}
-
-export function createShaderMaterial(fragShader: string) {
-	return new THREE.RawShaderMaterial({
-		vertexShader: passThruVert,
-		fragmentShader: fragShader
-	});
+export function createProgramInfo(fragShader?: string, vertShader?: string) {
+	return twgl.createProgramInfo(getWebGLContext(), [
+		fragShader ? fragShader : passThruFrag,
+		vertShader ? vertShader : passThruVert
+	]);
 }
 
 export function createTexture(width: number) {
-	const tex = new THREE.DataTexture(
-		new Uint8Array(width * width * 4),
-		width,
-		width,
-		THREE.RGBAFormat,
-		THREE.UnsignedByteType
-	);
-	tex.needsUpdate = true;
+	const gl = getWebGLContext();
+	if (!gl) return null;
+	const tex = gl.createTexture();
+	if (!tex) return null;
+	gl.bindTexture(gl.TEXTURE_2D, tex);
+	const level = 0;
+	const border = 0;
+	const internalFormat = gl.RGBA;
+	const format = gl.RGBA;
+	const type = gl.UNSIGNED_BYTE;
+	gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, width, border, format, type, null);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
 	return tex;
 }
 
-export function execute(rdr: THREE.WebGLRenderer, mat: THREE.ShaderMaterial, out: THREE.RenderTarget) {
-	MESH.material = mat;
-	rdr.setRenderTarget(out);
-	rdr.render(SCENE, CAMERA);
-	MESH.material = PASS_THRU_SHADER;
+export function createRenderTarget(texture: WebGLTexture) {
+	const gl = getWebGLContext();
+	if (!gl) return null;
+	const fb = gl.createFramebuffer();
+	if (!fb) return null;
+	gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+	return fb;
 }
 
-export function renderTexture(rdr: THREE.WebGLRenderer, input: THREE.Texture, width: number, out: THREE.RenderTarget) {
-	PASS_THRU_SHADER.uniforms.u_inputTexture = { value: input };
-	PASS_THRU_SHADER.uniforms.u_outputWidth = { value: width };
-	execute(rdr, PASS_THRU_SHADER, out);
+const BUFFER_INFO = twgl.createBufferInfoFromArrays(getWebGLContext(), {
+	position: {
+		data: [-1, -1, 1, -1, -1, 1, -1, 1, 1, -1, 1, 1],
+		numComponents: 2
+	},
+	texcoord: [0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1]
+});
+
+export interface Uniforms {
+	[key: string]: number | WebGLTexture;
 }
 
-export function createRenderTarget(width: number) {
-	return new THREE.WebGLRenderTarget(width, width, {
-		wrapS: THREE.ClampToEdgeWrapping,
-		wrapT: THREE.ClampToEdgeWrapping,
-		minFilter: THREE.NearestFilter,
-		magFilter: THREE.NearestFilter,
-		format: THREE.RGBAFormat,
-		type: THREE.UnsignedByteType,
-		depthBuffer: false,
-		stencilBuffer: false
+export function execute(program: twgl.ProgramInfo, width: number, framebuffer: WebGLFramebuffer, uniforms?: Uniforms) {
+	const gl = getWebGLContext();
+	gl.useProgram(program);
+	gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+	twgl.setBuffersAndAttributes(gl, program, BUFFER_INFO);
+	twgl.setUniforms(program, uniforms ? uniforms : {});
+	gl.viewport(0, 0, width, width);
+	gl.drawArrays(gl.TRIANGLES, 0, 6);
+}
+
+const PASS_THRU_SHADER_PROGRAM = createProgramInfo(passThruFrag);
+
+export function renderTexture(input: WebGLTexture, width: number, framebuffer: WebGLFramebuffer) {
+	execute(PASS_THRU_SHADER_PROGRAM, width, framebuffer, {
+		u_inputTexture: { value: input },
+		u_outputWidth: { value: width }
 	});
 }
+
+export function readTargetPixels(width: number, framebuffer: WebGLFramebuffer) {
+	const arr = new Uint8Array(width * width * 4);
+	const gl = getWebGLContext();
+	if (!gl) {
+		console.error("Unable to get WebGL context.");
+		return arr;
+	}
+	if (gl.checkFramebufferStatus(gl.FRAMEBUFFER) !== gl.FRAMEBUFFER_COMPLETE) {
+		console.error("Framebuffer is not complete.");
+		return arr;
+	}
+	gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+	gl.readPixels(0, 0, width, width, gl.RGBA, gl.UNSIGNED_BYTE, arr);
+	return arr;
+}
+
+// const CAMERA = new THREE.Camera();
+// CAMERA.position.z = 1;
+// const PASS_THRU_SHADER = createShaderMaterial(passThruFrag) as THREE.RawShaderMaterial
+// PASS_THRU_SHADER.uniforms.texture = { value: null };
+// const SCENE = new THREE.Scene();
+// const MESH = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2), PASS_THRU_SHADER);
+// SCENE.add(MESH);
+
+// export function testCapabilities(rdr: THREE.WebGLRenderer) {
+// 	return null;
+// }
+
+// export function createShaderMaterial(fragShader: string) {
+// 	return new THREE.RawShaderMaterial({
+// 		vertexShader: passThruVert,
+// 		fragmentShader: fragShader
+// 	});
+// }
+
+// export function createTexture(width: number) {
+// 	const tex = new THREE.DataTexture(
+// 		new Uint8Array(width * width * 4),
+// 		width,
+// 		width,
+// 		THREE.RGBAFormat,
+// 		THREE.UnsignedByteType
+// 	);
+// 	tex.needsUpdate = true;
+// 	return tex;
+// }
+
+// export function execute(rdr: THREE.WebGLRenderer, mat: THREE.ShaderMaterial, out: THREE.RenderTarget) {
+// 	MESH.material = mat;
+// 	rdr.setRenderTarget(out);
+// 	rdr.render(SCENE, CAMERA);
+// 	MESH.material = PASS_THRU_SHADER;
+// }
+
+// export function renderTexture(rdr: THREE.WebGLRenderer, input: THREE.Texture, width: number, out: THREE.RenderTarget) {
+// 	PASS_THRU_SHADER.uniforms.u_inputTexture = { value: input };
+// 	PASS_THRU_SHADER.uniforms.u_outputWidth = { value: width };
+// 	execute(rdr, PASS_THRU_SHADER, out);
+// }
+
+// export function createRenderTarget(width: number) {
+// 	return new THREE.WebGLRenderTarget(width, width, {
+// 		wrapS: THREE.ClampToEdgeWrapping,
+// 		wrapT: THREE.ClampToEdgeWrapping,
+// 		minFilter: THREE.NearestFilter,
+// 		magFilter: THREE.NearestFilter,
+// 		format: THREE.RGBAFormat,
+// 		type: THREE.UnsignedByteType,
+// 		depthBuffer: false,
+// 		stencilBuffer: false
+// 	});
+// }
--- src/logic/Main.ts
+++ src/logic/Main.ts
@@ -5,7 +5,6 @@ import { unpackInt16, packInt16, MIN_INT16, MAX_INT16 } from "./VectorInt16";
 import { unpackBooleans, packBooleans } from "./VectorBoolArray";
 import * as GPGPU from "./GPGPU";
 import { uniformInt32Range } from "./NotRandom";
-import * as THREE from "three";
 
 import horizSortEvenOdd from "!!raw-loader!./01_horizSortEvenOdd.frag";
 import horizSortOddEven from "!!raw-loader!./02_horizSortOddEven.frag";
@@ -14,26 +13,37 @@ import vertSortOddEven from "!!raw-loader!./04_vertSortOddEven.frag";
 
 export const textureWidth = 1024;
 
-const renderer = new THREE.WebGLRenderer();
-const firstShader = GPGPU.createShaderMaterial(horizSortEvenOdd);
-const secondShader = GPGPU.createShaderMaterial(horizSortOddEven);
-const thirdShader = GPGPU.createShaderMaterial(vertSortEvenOdd);
-const fourthShader = GPGPU.createShaderMaterial(vertSortOddEven);
-const gpuSortedObjectsTargets = [GPGPU.createRenderTarget(textureWidth), GPGPU.createRenderTarget(textureWidth)];
+const firstShader = GPGPU.createProgramInfo(horizSortEvenOdd);
+const secondShader = GPGPU.createProgramInfo(horizSortOddEven);
+const thirdShader = GPGPU.createProgramInfo(vertSortEvenOdd);
+const fourthShader = GPGPU.createProgramInfo(vertSortOddEven);
+const gpuSortedObjectsTargets = [
+	GPGPU.createRenderTarget(textureWidth),
+	GPGPU.createRenderTarget(textureWidth)
+] as WebGLFramebuffer[];
+
+// const renderer = new THREE.WebGLRenderer();
+// const firstShader = GPGPU.createShaderMaterial(horizSortEvenOdd);
+// const secondShader = GPGPU.createShaderMaterial(horizSortOddEven);
+// const thirdShader = GPGPU.createShaderMaterial(vertSortEvenOdd);
+// const fourthShader = GPGPU.createShaderMaterial(vertSortOddEven);
+// const gpuSortedObjectsTargets = [GPGPU.createRenderTarget(textureWidth), GPGPU.createRenderTarget(textureWidth)];
 
 export function initialize() {
 	console.log("initializing");
 	const inputTex = GPGPU.createTexture(textureWidth);
-	for (var i = 0; i < inputTex.image.data.length; i += 4) {
+	if (!inputTex) return;
+	const arr = new Uint8Array(textureWidth * textureWidth * 4);
+	for (var i = 0; i < arr.length; i += 4) {
 		const xArr = packInt16(uniformInt32Range(i, MIN_INT16, MAX_INT16));
 		const yArr = packInt16(uniformInt32Range(i + 123, MIN_INT16, MAX_INT16));
-		inputTex.image.data[i + 0] = xArr[0];
-		inputTex.image.data[i + 1] = xArr[1];
-		inputTex.image.data[i + 2] = yArr[0];
-		inputTex.image.data[i + 3] = yArr[1];
+		arr[i + 0] = xArr[0];
+		arr[i + 1] = xArr[1];
+		arr[i + 2] = yArr[0];
+		arr[i + 3] = yArr[1];
 	}
-	console.log(inputTex.image.data);
-	GPGPU.renderTexture(renderer, inputTex, textureWidth, gpuSortedObjectsTargets[0]);
+	console.log(arr);
+	GPGPU.renderTexture(inputTex, textureWidth, gpuSortedObjectsTargets[0]);
 }
 
 let z = 0;
@@ -44,21 +54,15 @@ const switchIndex = () => {
 };
 
 export function renderFrame() {
-	firstShader.uniforms.u_gpuSortedObjects = { value: gpuSortedObjectsTargets[switchIndex()].texture };
-	GPGPU.execute(renderer, firstShader, gpuSortedObjectsTargets[z]);
-	secondShader.uniforms.u_gpuSortedObjects = { value: gpuSortedObjectsTargets[switchIndex()].texture };
-	GPGPU.execute(renderer, secondShader, gpuSortedObjectsTargets[z]);
-	thirdShader.uniforms.u_gpuSortedObjects = { value: gpuSortedObjectsTargets[switchIndex()].texture };
-	GPGPU.execute(renderer, thirdShader, gpuSortedObjectsTargets[z]);
-	fourthShader.uniforms.u_gpuSortedObjects = { value: gpuSortedObjectsTargets[switchIndex()].texture };
-	GPGPU.execute(renderer, fourthShader, gpuSortedObjectsTargets[z]);
+	GPGPU.execute(firstShader, textureWidth, gpuSortedObjectsTargets[z], {
+		u_gpuSortedObjects: { value: gpuSortedObjectsTargets[switchIndex()] }
+	});
 }
 
 export function getBitmapImage() {
-	const gpuBytes = new Uint8Array(textureWidth * textureWidth * 4);
-	renderer.readRenderTargetPixels(gpuSortedObjectsTargets[z], 0, 0, textureWidth, textureWidth, gpuBytes);
-	const bmpBytes = new Uint8Array(textureWidth * textureWidth * 4);
-	for (var i = 0; i < gpuBytes.length; i += 4) {
+	const gpuBytes = GPGPU.readTargetPixels(textureWidth, gpuSortedObjectsTargets[z]);
+	const bmpBytes = new Uint8Array(gpuBytes.length);
+	for (var i = 0; i < bmpBytes.length; i += 4) {
 		const x = unpackInt16(gpuBytes[i + 0], gpuBytes[i + 1]);
 		const y = unpackInt16(gpuBytes[i + 2], gpuBytes[i + 3]);
 		bmpBytes[i + 0] = 256 * ((x + 32767) / 65536);
