c6fcdfa62953fe99475beb82a7201ed39f145f69
Alex Demskie <alex.demskie@bigfishgames.com>
2019-08-08 19:21:12 -0700

    working prototype

--- src/logic/Main.ts
+++ src/logic/Main.ts
@@ -3,7 +3,8 @@
 
 import { unpackInt16, packInt16, MIN_INT16, MAX_INT16 } from "./VectorInt16";
 import { unpackBooleans, packBooleans } from "./VectorBoolArray";
-import * as GPGPU from "./GPGPU";
+import { ComputeShader } from "./ComputeShader";
+import { RenderTarget } from "./RenderTarget";
 import { uniformInt32Range } from "./NotRandom";
 
 import horizSortEvenOdd from "!!raw-loader!./01_horizSortEvenOdd.frag";
@@ -12,42 +13,43 @@ import vertSortEvenOdd from "!!raw-loader!./03_vertSortEvenOdd.frag";
 import vertSortOddEven from "!!raw-loader!./04_vertSortOddEven.frag";
 
 export const textureWidth = 1024;
+export const cyclesPerFrame = 16;
 
-const firstShader = GPGPU.createProgram(horizSortEvenOdd);
-const secondShader = GPGPU.createProgram(horizSortOddEven);
-const thirdShader = GPGPU.createProgram(vertSortEvenOdd);
-const fourthShader = GPGPU.createProgram(vertSortOddEven);
-const targets = [new GPGPU.FBO(textureWidth), new GPGPU.FBO(textureWidth)];
+const renderTarget = new RenderTarget(textureWidth);
+const fragVariables = { textureWidth: `${textureWidth}.0` };
+const firstShader = new ComputeShader(horizSortEvenOdd, fragVariables);
+const secondShader = new ComputeShader(horizSortOddEven, fragVariables);
+const thirdShader = new ComputeShader(vertSortEvenOdd, fragVariables);
+const fourthShader = new ComputeShader(vertSortOddEven, fragVariables);
 
 export function initialize() {
 	console.log("initializing");
-	const inputFBO = targets[0];
-	if (!inputFBO) return;
-	const arr = new Uint8Array(textureWidth * textureWidth * 4);
-	for (var i = 0; i < arr.length; i += 4) {
+	const bytes = new Uint8Array(textureWidth * textureWidth * 4);
+	for (var i = 0; i < bytes.length; i += 4) {
 		const xArr = packInt16(uniformInt32Range(i, MIN_INT16, MAX_INT16));
 		const yArr = packInt16(uniformInt32Range(i + 123, MIN_INT16, MAX_INT16));
-		arr[i + 0] = xArr[0];
-		arr[i + 1] = xArr[1];
-		arr[i + 2] = yArr[0];
-		arr[i + 3] = yArr[1];
+		bytes[i + 0] = xArr[0];
+		bytes[i + 1] = xArr[1];
+		bytes[i + 2] = yArr[0];
+		bytes[i + 3] = yArr[1];
 	}
-	console.log(arr);
-	inputFBO.modifyTexture(arr);
+	console.log("pushing");
+	renderTarget.pushTextureData(bytes);
 }
 
-let z = 0;
-const nextIndex = () => (z = (z + 1) % 2);
-
 export function renderFrame() {
-	GPGPU.execute(firstShader, targets[1], { u_gpuSortedObjects: targets[0].texture });
-	GPGPU.execute(secondShader, targets[0], { u_gpuSortedObjects: targets[1].texture });
-	GPGPU.execute(thirdShader, targets[1], { u_gpuSortedObjects: targets[0].texture });
-	GPGPU.execute(fourthShader, targets[0], { u_gpuSortedObjects: targets[1].texture });
+	console.log("computing");
+	for (var i = 0; i < cyclesPerFrame; i++) {
+		renderTarget.compute(firstShader, { u_gpuSortedObjects: renderTarget });
+		renderTarget.compute(secondShader, { u_gpuSortedObjects: renderTarget });
+		renderTarget.compute(thirdShader, { u_gpuSortedObjects: renderTarget });
+		renderTarget.compute(fourthShader, { u_gpuSortedObjects: renderTarget });
+	}
 }
 
 export function getBitmapImage() {
-	const gpuBytes = targets[0].readPixels();
+	console.log("reading");
+	const gpuBytes = renderTarget.readPixels();
 	const bmpBytes = new Uint8Array(gpuBytes.length);
 	for (var i = 0; i < bmpBytes.length; i += 4) {
 		const x = unpackInt16(gpuBytes[i + 0], gpuBytes[i + 1]);
